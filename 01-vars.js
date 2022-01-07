7abb842695ed289300070582c66e1413d3d1a30b
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-01-07 12:09:21 +0200

    Тренировка js

--- 01-vars.js
+++ 01-vars.js
@@ -1,3 +1,79 @@
-console.log('555');
-const string1 = 'привет';
-console.log(string1);    
+// "use strict";
+// console.log('555');
+// const string1 = 'привет';
+// console.log(string1);
+// // alert('Я JavaScript!');
+// let message = "Hello!!";
+// alert(message);
+
+
+// let name = 'Джон';
+// let admin = 'Джон';
+// alert(admin);
+
+// const planetEarth = 'planet Earth';
+// const currentUser = 'Name current user';
+// alert(planetEarth);
+// alert("не число" / 2);
+
+// let name = "Иван";
+// // Вставим переменную
+// alert( `Привет, ${name}!` ); // Привет, Иван!
+// // Вставим выражение
+// alert( `результат: ${1 + 2}` ); // результат: 3
+
+// const message = "JavaScript is awesome!";
+// console.log(message);
+// alert(message);
+// const username = "Mango";
+// console.log("Username is ", username);
+
+
+// console.log('%c Auth ', 
+//             'color: white; background-color: #2274A5', 
+//             'Login page rendered');
+// console.log('%c GraphQL ', 
+//             'color: white; background-color: #95B46A', 
+//             'Get user details');
+// console.log('%c Error ', 
+//             'color: white; background-color: #D33F49', 
+//     'Error getting user details');
+            
+
+
+    // Просим клиента подтвердить бронь на отель
+// и сохраняем в переменную результат работы confirm
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+
+// Спрашиваем имя отеля в котором хотел бы остановится клиент
+// и сохраняем в переменную результат вызова prompt.
+// const hotelName = prompt("Please enter desired hotel name");
+// console.log(hotelName);
+
+
+const x = 10;
+const y = 5;
+
+// Сложение
+console.log(x + y); // 15
+
+// Вычитание
+console.log(x - y); // 5
+
+// Умножение
+console.log(x * y); // 50
+
+// Деление
+console.log(x / y); // 2
+
+// Остаток от деления
+console.log(x % y); // 0
+
+// Сложение с заменой (также есть для всех других операторов)
+let value = 5;
+
+// Аналогично записи value = value + 10;
+value += 10;
+console.log(value); // 15
